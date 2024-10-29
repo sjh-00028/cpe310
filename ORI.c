7725bc13fc88838cf5d85c5594c87f8022c9701e
sjh-00028 <sjh00028@mix.wvu.edu>
2024-10-29 11:33:28 -0400

    Implement `ORI`

--- ORI.c
+++ ORI.c
@@ -1,11 +1,49 @@
 #include "Instruction.h"
 
 void ori_immd_assm(void) {
-	state = WRONG_COMMAND;
+	if (strcmp(OP_CODE, "ORI") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	if (PARAM1.type != REGISTER || PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	} else if (PARAM3.type != IMMEDIATE) {
+		state = INVALID_PARAM;
+		return;
+	}
+
+	if (PARAM1.value > 31 || PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	} else if (PARAM3.value > 0xFFFF) {
+		state = INVALID_IMMED;
+		return;
+	}
+
+	setBits_num(31, 13, 6);
+	setBits_num(20, PARAM1.value, 5);
+	setBits_num(25, PARAM2.value, 5);
+	setBits_num(15, PARAM3.value, 16);
+	state = COMPLETE_ENCODE;
 }
 
 void ori_immd_bin(void) {
-	state = WRONG_COMMAND;
+	if (checkBits(31, "001101") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	uint32_t Rt = getBits(20, 5);
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Immed = getBits(15, 16);
+
+	setOp("ORI");
+	setParam(1, REGISTER, Rt);
+	setParam(2, REGISTER, Rs);
+	setParam(3, IMMEDIATE, Immed);
+	state = COMPLETE_DECODE;
 }
 
 
