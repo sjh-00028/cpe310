0da551a6917f4d2017bf6e55bca716a131653de3
sjh-00028 <sjh00028@mix.wvu.edu>
2024-11-08 19:34:43 -0500

    Fix ADD

--- ADD.c
+++ ADD.c
@@ -48,7 +48,7 @@ void add_reg_bin(void) {
 	// check_bits(start_bit, bit_string) returns 0 if the bit_string matches
 	//  any x will be skipped
 	// If the manual shows (0), then the value of that bit doesnt matter
-	if (checkBits(31, "000000") != 0 || checkBits(9, "00000100000") != 0) {
+	if (checkBits(31, "000000") != 0 || checkBits(5, "100000") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
